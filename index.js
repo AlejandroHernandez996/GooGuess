ab37be2bd11517d2cd3a05c5d3639e6dd9159ab9
Alejandro Hernandez <alejandrohernandez996@gmail.com>
2017-09-19 22:52:15 -0500

    fds

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ var io = require('socket.io')(http);
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
-var port = 8080 || process.env.PORT;
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
 var textURL = '';
 var imgURL = '';
 
@@ -82,6 +84,6 @@ io.on('connection', function(socket){
         io.emit('chat message', msg);
   });
 });
-http.listen(port,'0.0.0.0', function(){
+http.listen(port, ip, function(){
   console.log('listening on *:3000');
 });
