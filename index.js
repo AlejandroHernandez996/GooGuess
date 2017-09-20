22de206c9c3187745ba1547f4fb312843bcd7646
Alejandro Hernandez <alejandrohernandez996@gmail.com>
2017-09-19 22:25:25 -0500

    init

--- index.js
+++ index.js
@@ -13,9 +13,7 @@ var title ='Google';
 
 var key = 'Google';
 
-http.listen(port, function(){
-  console.log('listening on *:3000');
-});
+
 //Home page is index.html
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -83,4 +81,7 @@ io.on('connection', function(socket){
     else
         io.emit('chat message', msg);
   });
+});
+http.listen(port, function(){
+  console.log('listening on *:3000');
 });
